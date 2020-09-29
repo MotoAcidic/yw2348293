5f4f4311ff214f40b88795cc4b2a7b959705809a
Big-Sock <bigsock666@gmail.com>
2020-09-28 22:41:39 -0700

    cors

--- server/server.js
+++ server/server.js
@@ -8,12 +8,19 @@ const dotenv = require('dotenv')
 const Web3 = require('web3');
 const cors = require('cors')
 const connectDB = require('./db')
-dotenv.config()
-connectDB()
+
 
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
-
+dotenv.config()
+connectDB()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api', web3Router)

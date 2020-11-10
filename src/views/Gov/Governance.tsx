154d89ab1c2dfb143f2a7541386b4039c4a53739
BigFluff666 <ezynellgaming@gmail.com>
2020-11-09 21:29:07 -0700

    work on gov profile

--- src/views/Gov/Governance.tsx
+++ src/views/Gov/Governance.tsx
@@ -10,6 +10,16 @@ import useYam from '../../hooks/useYam'
 import { OverviewData } from '../Home/types'
 import { getStats } from '../Home/utils'
 import TopDisplayContainer from "../../components/TopDisplayContainer";
+import Profile from "./Profile";
+
+function isMobile() {
+  if (window.innerWidth < window.innerHeight) {
+    return true
+  }
+  else {
+    return false
+  }
+}
 
 function getServerURI() {
   if (window.location.hostname === "localhost") {
@@ -18,7 +28,7 @@ function getServerURI() {
   return "https://yieldwars-api.herokuapp.com";
 }
 
-const Vote: React.FC = () => {
+const Governance: React.FC = () => {
 
   const { account, connect } = useWallet()
   const yam = useYam()
@@ -35,20 +45,9 @@ const Vote: React.FC = () => {
     setStats(statsData)
   }, [yam, setStats])
 
-  const fetchAccount = () => {
-    axios.post(`${getServerURI()}/gov/get-account`,
-      { address: account, }).then(res => {
-        console.log("user", res.data);
-      }).catch(err => {
-        console.log(err);
-      })
-    console.log("my acct", account);
-  }
-
   useEffect(() => {
     if (yam) {
       fetchStats()
-      fetchAccount()
     }
   }, [yam])
 
@@ -59,13 +58,127 @@ const Vote: React.FC = () => {
         <ContentContainer>
           <Page>
             <TopDisplayContainer />
+            <Leaderboard>
+              Leaderboard
+              </Leaderboard>
+            {yam ?
 
+              <GovContent>
+                <Suggestions>
+                  Suggestions
+              </Suggestions>
+                <Profile />
+              </GovContent>
+              :
+              <ConnectContainer onClick={() => connect('injected')}>
+                <BigTitle>
+                  Connect Your Wallet
+          </BigTitle>
+                <SubTitle>
+                  to participate in governance
+                </SubTitle>
+              </ConnectContainer>
+            }
           </Page>
         </ContentContainer>
       </StyledCanvas>
     </Switch>
   )
 }
+
+const SubTitle = styled.div`
+font-family: "Gilroy";
+font-size: 20px;
+font-weight: bold;
+font-stretch: normal;
+font-style: normal;
+line-height: 1;
+letter-spacing: normal;
+	color: white;
+	margin-bottom: 10px;
+`
+
+const BigTitle = styled.div`
+font-family: "Gilroy";
+  font-size: 50px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1;
+  letter-spacing: normal;
+  color: rgb(255, 204, 74);
+  max-width: 80vw;
+	margin: 0 auto 20px auto;
+	display: flex;
+	align-items: center;
+`
+
+const ConnectContainer = !isMobile() ? styled.div`
+width: 600px;
+height: 140px;
+display: flex;
+align-items: center;
+justify-content: center;
+font-size: 30px;
+margin: 0 auto 5vh auto;
+font-family: "Gilroy";
+font-weight: bold;
+font-stretch: normal;
+font-style: normal;
+line-height: 1;
+letter-spacing: normal;
+flex-direction: column;
+color: #ffffff;
+border-radius: 8px;
+border: solid 2px rgba(255, 183, 0, 0.3);
+ background-color: rgba(4,2,43,0.4);
+ cursor: pointer;
+` : styled.div`
+margin: 0 0 40px 0;
+width: 90vw;
+display: flex;
+flex-direction: column;
+padding-top: 20px;
+font-family: "Gilroy";
+  font-size: 25px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1;
+  letter-spacing: normal;
+	color: #ffffff;
+	border-radius: 8px;
+  border: solid 2px rgba(255, 183, 0, 0.3);
+ background-color: rgba(4,2,43,0.4); 
+`
+
+
+const Suggestions = styled.div`
+width: 59%;
+height: 500px;
+background-color: rgba(256,256,256,0.1);
+border-radius: 16px;
+display: flex;
+align-items: center;
+justify-content: center;
+`
+
+const Leaderboard = styled.div`
+width: 80vw;
+height: 175px;
+background-color: rgba(256,256,256,0.1);
+border-radius: 16px;
+display: flex;
+align-items: center;
+justify-content: center;
+margin-bottom: 80px;
+`
+
+const GovContent = styled.div`
+width: 80vw;
+display: flex;
+flex-direction: row;
+justify-content: space-between;`
 
 const BackgroundSection = styled.div`
   background-image: url(${Background});
@@ -88,4 +201,4 @@ const ContentContainer = styled.div`
   text-align: center;
 `;
 
-export default Vote
+export default Governance

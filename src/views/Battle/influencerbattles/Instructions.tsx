0b59ffc8ffad526d124b9c0fe86c8cf7ff1587ec
BigFluff666 <ezynellgaming@gmail.com>
2020-11-16 12:47:09 -0700

    rules

--- src/views/Battle/influencerbattles/Instructions.tsx
+++ src/views/Battle/influencerbattles/Instructions.tsx
@@ -102,7 +102,7 @@ const Instructions: React.FC = () => {
         <MilestoneRow>
           <img className="milestone-image" alt="milestone" src={img_2} />
           <MilestoneText>
-          Vote daily to earn rewards. Battles start at 19:00 UTC and last for 24hours. If you unstake during a battle, you will not earn rewards and your vote will not count.
+          Vote daily to earn rewards. Battles start at 20:00 UTC and last for 23hours. If you unstake during a battle, you will not earn rewards and your vote will not count.
               </MilestoneText>
         </MilestoneRow>
         {/* <MilestoneRow>
@@ -162,7 +162,7 @@ Once a bet is placed it is locked in, it cannot be withdrawn or changed.
         <MilestoneRow>
           <img className="milestone-image" alt="milestone" src={img_3} />
           <MilestoneText>
-            Battles and Bets last 24 hours, starting at 8:00 PM UTC and ending at 7:00 PM UTC the next day.
+            Bets last 17 hours, starting at 20:00 UTC and ending at 13:00 UTC the next day.
               </MilestoneText>
         </MilestoneRow>
         <MilestoneRow>
